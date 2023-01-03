bace410dfb42c50498a0eed00c9b5624429ea067
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-01-03 10:24:56 -0500

    updated validations and user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :discussions, through: :comments
     has_secure_password 
+    validates :username, presence: :true
 end
