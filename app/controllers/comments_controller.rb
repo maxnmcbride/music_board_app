739dc78aef2fa8e2180555e170f9aa1b749dc3c0
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-01-03 15:45:56 -0500

    updating CRUD on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,32 @@
 class CommentsController < ApplicationController
     # have to develop full CRUD capability for users to post, read, edit, and delete comments
+    
     def create
         comment = Comment.create!(comments_params)
         render json: comment, status: :created
     end
 
+    def index
+    end
+
+    # def edit or update?
+    # end
+
+    def destroy
+        comment = Comment.find_by(id: params[:id])
+        if comment
+            comment.destroy
+            head :no_content
+        else
+            render json: {error: "Comment not found"}, status: :not_found
+        end
+    end
+
+
     private
 
     def comments_params
-        params.permit(:post)
+        params.permit(:post, :user_id, :discussion_id)
     end
 
 # get    '/posts',          to: 'posts#index'   # all posts
