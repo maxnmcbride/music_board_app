4bc57498b86d589c89cc46655d9a70ded2b7c127
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-01-03 12:24:13 -0500

    working on comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,28 @@
 class CommentsController < ApplicationController
     # have to develop full CRUD capability for users to post, read, edit, and delete comments
+    def create
+        comment = Comment.create!(comments_params)
+        render json: comment, status: :created
+    end
+
+    private
+
+    def comments_params
+        params.permit(:post)
+    end
+
+# get    '/posts',          to: 'posts#index'   # all posts
+# get    '/posts/new',      to: 'posts#new'     # form to create a post
+# post   '/posts',          to: 'posts#create'  # create a post from a form submission
+# get    '/posts/:id',      to: 'posts#show'    # show a single post
+# get    '/posts/:id/edit', to: 'posts#edit'    # form to edit a post
+# put    '/posts/:id',      to: 'posts#update'  # for legacy compatibility 
+# patch  '/posts/:id',      to: 'posts#update'  # update a post from a form submission
+# delete '/posts/:id',      to: 'posts#destroy' # delete a post
+
+
+# def show
+# render json: @current_user
+# end
+
 end
--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,3 +1,4 @@
 class DiscussionsController < ApplicationController
     # develop full CRUD capabilities for users read, edit, create, and delete discussion boards
+    
 end
